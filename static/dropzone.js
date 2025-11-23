1825cb4e9837103ab92ded11aa88ce528397bd21
Md Munimul Islam <mishyket127@gmail.com>
2025-11-23 05:16:29 +0000

    Added Uploaded Image preview in client side

--- static/dropzone.js
+++ static/dropzone.js
@@ -2,8 +2,14 @@ const dropZone = document.getElementById('dropZone');
 const fileInput = document.getElementById('file');
 const fileName = document.getElementById('fileName');
 const submitBtn = document.getElementById('submitBtn');
+const imagePreview = document.getElementById('image-preview');
+const imageHolder = document.getElementById('image-holder');
 
-// Drag and drop events
+const allowedFileTypes = ["image/png", "image/jpeg", "image/gif"];
+
+const allowedFileSizeInMB = 50
+const allowedFileSizeInBytes = allowedFileSizeInMB * 1024 * 1024
+
 dropZone.addEventListener('dragover', (e) => {
     e.preventDefault();
     dropZone.classList.add('bg-primary', 'bg-opacity-10', 'border-primary');
@@ -16,35 +22,68 @@ dropZone.addEventListener('dragleave', () => {
 dropZone.addEventListener('drop', (e) => {
     e.preventDefault();
     dropZone.classList.remove('bg-primary', 'bg-opacity-10', 'border-primary');
+
     const files = e.dataTransfer.files;
+
     if (files.length > 0) {
         fileInput.files = files;
-        updateFileName();
     }
 });
 
-// File input change event
-fileInput.addEventListener('change', updateFileName);
+fileInput.addEventListener('change', (e) => {
+    files = fileInput.files;
+    isValid = validateUpload(files);
 
-cameraInput.addEventListener('change', (e) => {
-    const files = e.target.files;
-    if (files && files.length > 0) {
-        const file = files[0];
-        // Copy camera file to main file input
-        fileInput.files = files;
-        showPreview(file);
-    } else {
-        clearPreview();
-    }
-});
-
-function updateFileName() {
-    if (fileInput.files.length > 0) {
-        fileName.textContent = `✓ Selected: ${fileInput.files[0].name}`;
-        fileName.classList.remove('d-none');
-        submitBtn.disabled = false;
-    } else {
+    if (!isValid) {
         fileName.classList.add('d-none');
+        imageHolder.classList.add('d-none');
         submitBtn.disabled = true;
+
+        return;
     }
+
+    updateFileName(files[0].name);
+    updateImage(files[0]);
+});
+
+function validateUpload(files) {
+    isValid = true;
+
+    if (files.length > 1) {
+        console.log("Only 1 image can be uploaded at a time")
+        isValid = false;
+    }
+
+    file = files[0];
+
+    if (!allowedFileTypes.includes(file.type)) {
+        console.log("Please upload image file")
+        isValid = false;
+    }
+
+    if (file.size > allowedFileSizeInBytes) {
+        console.log("file size cant be more than " + allowedFileSizeInMB + "MB")
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function updateFileName(filename) {
+    fileName.textContent = `✓ Selected: ${filename}`;
+    fileName.classList.remove('d-none');
+    submitBtn.disabled = false;
+}
+
+function updateImage(file) {    
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        imagePreview.classList.remove('d-none');
+        imagePreview.src = e.target?.result;
+
+        imageHolder?.classList.remove('d-none');
+    };
+
+    reader.readAsDataURL(file);
 }
